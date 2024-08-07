a5b1774cf2f380b763bfff71ad4991c9eab1c45a
vovka <vovka1998@gmail.com>
2024-08-07 22:23:58 +0300

    add full interface for logger

--- config/interface.py
+++ config/interface.py
@@ -1,5 +1,16 @@
-from typing import TypedDict
-from logging.handlers import Handler
+from typing import TypedDict, List
+from enum import Enum
+from logging import Handler
+from os.path import Path
+
+
+class LevelLoggers(Enum):
+    DEBUG = 'DEBUG'
+    INFO = 'INFO'
+    WARNING = 'WARNING'
+    ERROR = 'ERROR'
+    CRITICAL = 'CRITICAL'
+
 
 class FormatFormatterLogger(TypedDict):
     format: str
@@ -8,20 +19,28 @@ class FormatterLogger(TypedDict):
     name_format: FormatFormatterLogger
 
 class SettingsHandlerLogger(TypedDict):
-    # class_handler:
+    class_handler: Handler
     when: str | None
     interval: int | None
     backupCount: int | None
-    level: None
+    level: LevelLoggers
     formatter: FormatterLogger
-    # filename:
+    filename: str
     encoding = 'utf-8'
 
 class HandlerLogger(TypedDict):
-    name_handler: str
+    name_handler: SettingsHandlerLogger
+
+class SettighsLoger(TypedDict):
+    level:LevelLoggers
+    handlers: List[HandlerLogger, ...]
+
+class LoggerConfig(TypedDict):
+    name_logger: SettighsLoger
 
 class LoggerDictConfig(TypedDict):
     version: int
     disable_existing_loggers: bool
     formatters: FormatterLogger
     handlers: HandlerLogger
+    loggers: LoggerConfig
